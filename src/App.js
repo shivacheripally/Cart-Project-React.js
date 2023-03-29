1d1c94091e94e24b02135d516b012708f9611550
shiva <shivacheripally14@gmail.com>
2023-03-29 19:20:33 +0530

    setstate is moved into app.js to use for child elements

--- src/App.js
+++ src/App.js
@@ -2,13 +2,96 @@ import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <Cart/>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      products: [
+        {
+          title: 'watch',
+          price: '99',
+          img: '',
+          qty: 1,
+          id: 1
+        },
+        {
+          title: 'Mobile Phone',
+          price: '999',
+          img: '',
+          qty: 10,
+          id: 2
+        },
+        {
+          title: 'Laptop',
+          price: '1000',
+          img: '',
+          qty: 4,
+          id: 3
+        }
+      ]
+    }
+  }
+  handleIncreaseQuantity = (product) => {
+    const { products } = this.state;
+
+    const index = products.indexOf(product);
+
+    products[index].qty += 1;
+
+    this.setState({
+      products: products
+    });
+  }
+  handleDecreaseQuantity = (product) => {
+    const { products } = this.state;
+
+    const index = products.indexOf(product);
+
+    if (products[index].qty === 0) return;
+
+    products[index].qty -= 1;
+
+    this.setState({
+      products: products
+    });
+  }
+
+  handleDeleteProduct = (id) => {
+    const { products } = this.state;
+
+    const items = products.filter((item) => item.id !== id);
+
+    this.setState({
+      products: items
+    });
+  }
+
+  getCartCount= ()=> {
+    const {products} = this.state;
+    let count = 0;
+
+    products.forEach(product => {
+      count += product.qty;
+    });
+
+    return count;
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <div className="App">
+        <Navbar count={this.getCartCount()}/>
+        <Cart
+          products={products}
+          onIncreaseQuantity={this.handleIncreaseQuantity}
+          onDecreaseQuantity={this.handleDecreaseQuantity}
+          onDeleteProduct={this.handleDeleteProduct}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Navbar.js
+++ src/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = (props) => {
             src="https://cdn-icons-png.flaticon.com/512/3144/3144456.png" 
             alt="cart-icon" 
         />
-        <span style={styles.cartCount}>3</span>
+        <span style={styles.cartCount}>{props.count}</span>
       </div>
     </div>
   );
