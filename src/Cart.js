1027323f25b65a32d770de8430d97c60873d04be
shiva <shivacheripally14@gmail.com>
2023-03-29 17:19:02 +0530

    delete item from cart

--- src/Cart.js
+++ src/Cart.js
@@ -3,13 +3,85 @@ import CartItem from './CartItem';
 
 class Cart extends React.Component {
 
+    constructor(){
+        super();
+        this.state = {
+            products : [
+                {
+                    title:'watch',
+                    price:'99',
+                    img:'',
+                    qty:1,
+                    id : 1
+                },
+                {
+                    title:'Mobile Phone',
+                    price:'999',
+                    img:'',
+                    qty:10,
+                    id : 2
+                },
+                {
+                    title:'Laptop',
+                    price:'1000',
+                    img:'',
+                    qty:4,
+                    id : 3
+                }
+            ]
+        }
+    }
+    handleIncreaseQuantity = (product) => {
+        const {products} = this.state;
+
+        const index = products.indexOf(product);
+
+        products[index].qty += 1;
+
+        this.setState({
+            products:products
+        });
+    }
+    handleDecreaseQuantity = (product) => {
+        const {products} = this.state;
+
+        const index = products.indexOf(product);
+
+        if(products[index].qty === 0) return;
+
+        products[index].qty -= 1;
+
+        this.setState({
+            products:products
+        });
+    }
+
+    handleDeleteProduct = (id)=>{
+        const {products} = this.state;
+
+        const items = products.filter((item) => item.id !== id);
+
+        this.setState({
+            products:items
+        });
+    }
+
     render(){
+        const {products} = this.state;
         return(
             <div className='cart'>
-                <CartItem/>
-                <CartItem/>
-                <CartItem/> 
-                <CartItem/>
+                
+                {products.map((product)=>{
+                    return (
+                        <CartItem 
+                            product = {product}
+                            key = {product.id}
+                            onIncreaseQuantity = {this.handleIncreaseQuantity}
+                            onDecreaseQuantity = {this.handleDecreaseQuantity}
+                            onDeleteProduct= {this.handleDeleteProduct}
+                        />
+                    );
+                })}
             </div>
         );
     }
--- src/CartItem.js
+++ src/CartItem.js
@@ -2,35 +2,14 @@ import React from 'react';
 
 class CartItem extends React.Component {
 
-    constructor(){
-        super();
-        this.state = {
-            title:'Mobile Phone',
-            price:'1000',
-            img:'',
-            qty:1,
-        }
-    }
-
-    increaseQuantity = ()=>{
-        this.setState((prevState)=>{
-            return{
-                qty: prevState.qty+1
-            }
-        });
-    }
-
-    decreaseQuantity = ()=>{
-        const {qty} = this.state;
-        if(qty === 0) return;
-
-        this.setState( {
-           qty:this.state.qty-1
-        });
-    }
-
     render() {
-        const {title,price,qty} = this.state;
+        const {title,price,qty} = this.props.product;
+        const {
+            product, 
+            onIncreaseQuantity, 
+            onDecreaseQuantity,
+            onDeleteProduct
+        } = this.props;
         return (
             <div className="cart-item">
                 <div className="left-block">
@@ -42,9 +21,24 @@ class CartItem extends React.Component {
                     <div style={{ color: '#777' }}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* {buttons} */}
-                        <img onClick={this.increaseQuantity} src="https://t4.ftcdn.net/jpg/01/07/62/07/240_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg" alt="increase" className='action-icons'/>
-                        <img onClick={this.decreaseQuantity} src="https://t3.ftcdn.net/jpg/03/73/49/86/240_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" alt="decrese" className='action-icons'/>
-                        <img src="https://t4.ftcdn.net/jpg/03/73/50/09/240_F_373500918_7vISJB85YXvvu7SgnpktP752LWRrLzyI.jpg" alt="delete" className='action-icons'/>
+                        <img 
+                            onClick={() => onIncreaseQuantity(product)} 
+                            src="https://t4.ftcdn.net/jpg/01/07/62/07/240_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg" 
+                            alt="increase" 
+                            className='action-icons'
+                        />
+                        <img 
+                            onClick={() => onDecreaseQuantity(product)} 
+                            src="https://t3.ftcdn.net/jpg/03/73/49/86/240_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" 
+                            alt="decrese" 
+                            className='action-icons'
+                        />
+                        <img 
+                            onClick={() => onDeleteProduct(product.id)}
+                            src="https://t4.ftcdn.net/jpg/03/73/50/09/240_F_373500918_7vISJB85YXvvu7SgnpktP752LWRrLzyI.jpg" 
+                            alt="delete" 
+                            className='action-icons'
+                        />
                     </div>
                 </div>
             </div>
